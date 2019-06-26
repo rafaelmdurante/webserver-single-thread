fa9d0cdc074bcd3bab9544f58d790789615d092f
Rafael Miranda Durante <rafaelmdurante@gmail.com>
2019-06-26 20:29:38 +0100

    class webserver comments for development

--- src/org/academiadecodigo/whiledcards/webserver/WebServer.java
+++ src/org/academiadecodigo/whiledcards/webserver/WebServer.java
@@ -1,4 +1,36 @@
 package org.academiadecodigo.whiledcards.webserver;
 
 public class WebServer {
+
+    // Attributes
+    
+
+    // Main
+    public static void main(String[] args) {
+
+    }
+
+    // Flow
+    /*
+    - Listen
+    - Serve
+    - Dispatch
+        > create in/out
+        > fetchRequestHeaders
+        > separate verb/request/resource
+        ---- create header ---
+        > !GET notAllowed
+        > resource null BadRequest
+        > resource supported?
+        > resource exists? (ok / FileNotFound)
+        > reply filePath
+          reply length
+        ---- header end ----
+        > stream file
+        > close connection
+     */
+
+    // Methods
+
+
 }
